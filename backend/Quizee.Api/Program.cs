d93eda5d955ac7abe7f613706d5e30a6fd33adde
nileshmishra280 <nileshmishra2508@gmail.com>
2025-04-29 00:35:22 +0530

    port settled to 0000

--- backend/Quizee.Api/Program.cs
+++ backend/Quizee.Api/Program.cs
@@ -8,7 +8,7 @@ using Quizee.Api.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add this line after CreateBuilder
-builder.WebHost.UseUrls("http://localhost:80");
+builder.WebHost.UseUrls("http://0.0.0.0:80");
 
 // Add services to the container.
 builder.Services.AddControllers();
