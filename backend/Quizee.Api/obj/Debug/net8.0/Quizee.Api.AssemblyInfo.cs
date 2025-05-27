3333b78afcbfea4279f085f8e2cc5a12158b397a
nileshmishra280 <nileshmishra2508@gmail.com>
2025-05-27 18:08:43 +0530

    new DB ADDED

--- backend/Quizee.Api/obj/Debug/net8.0/Quizee.Api.AssemblyInfo.cs
+++ backend/Quizee.Api/obj/Debug/net8.0/Quizee.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Quizee.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16ddbb1e05d7d224af8c8ef5102fc846cbd7af58")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+685c0543e6095526b28bf1571a08c014e8fdf2fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Quizee.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Quizee.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
